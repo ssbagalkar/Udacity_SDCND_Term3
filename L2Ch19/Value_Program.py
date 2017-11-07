40aab337e351c78f2f55caae16484a71d2849df2
Saurabh S Bagalkar <sbagalka@asu.edu>
2017-11-06 21:53:40 -0600

    complete value program

--- L2Ch19/Value_Program.py
+++ L2Ch19/Value_Program.py
@@ -9,10 +9,10 @@
 # assign that cell a value of 99.
 # ----------
 
-grid = [[0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
+grid = [[0, 1, 0, 0, 1, 0],
+        [0, 1, 0, 0, 1, 0],
+        [0, 1, 0, 0, 1, 0],
+        [0, 1, 0, 0, 1, 0],
         [0, 0, 0, 0, 1, 0]]
 goal = [len(grid) - 1, len(grid[0]) - 1]
 cost = 1  # the cost associated with moving from a cell to an adjacent one
@@ -29,7 +29,51 @@ def compute_value(grid, goal, cost):
     # ----------------------------------------
     # insert code below
     # ----------------------------------------
+    closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
+    closed[goal[0]][goal[1]] = 1
 
-    # make sure your function returns a grid of values as
-    # demonstrated in the previous video.
-    return value
+    expand = [[99 for col in range(len(grid[0]))] for row in range(len(grid))]
+    expand[goal[0]][goal[1]] = 0
+
+    x = goal[0]
+    y = goal[1]
+    g = 0
+
+    open = [[g, x, y]]
+
+    found = False  # flag that is set when we process all cells
+    resign = False  # flag set if we can't find expand
+    #count = 0
+
+    while not found and not resign:
+        if len(open) == 0:
+            return expand
+        else:
+            open.sort()
+            open.reverse()
+            next = open.pop()
+            x = next[1]
+            y = next[2]
+            g = next[0]
+            #expand[x][y] = g
+            #count += 1
+
+            if x == grid[0][0] and y == grid[0][0]:
+                found = True
+            else:
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost
+                            open.append([g2, x2, y2])
+                            expand[x2][y2] = g2
+                            closed[x2][y2] = 1
+    #for i in range(len(grid)):
+     #   print(expand[i])
+    return expand
+
+expand = compute_value(grid, goal, cost)
+for i in range(len(expand)):
+    print(expand[i])
