9400dec5c39ac1abbb8a6905b0fc4e199dcd9af7
Saurabh S Bagalkar <sbagalka@asu.edu>
2017-11-06 22:36:06 -0600

    correct logic

--- L2Ch19/Value_Program.py
+++ L2Ch19/Value_Program.py
@@ -9,10 +9,10 @@
 # assign that cell a value of 99.
 # ----------
 
-grid = [[0, 1, 0, 0, 1, 0],
-        [0, 1, 0, 0, 1, 0],
-        [0, 1, 0, 0, 1, 0],
-        [0, 1, 0, 0, 1, 0],
+grid = [[0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 1, 1, 0],
         [0, 0, 0, 0, 1, 0]]
 goal = [len(grid) - 1, len(grid[0]) - 1]
 cost = 1  # the cost associated with moving from a cell to an adjacent one
@@ -58,18 +58,17 @@ def compute_value(grid, goal, cost):
             #expand[x][y] = g
             #count += 1
 
-            if x == grid[0][0] and y == grid[0][0]:
-                found = True
-            else:
-                for i in range(len(delta)):
-                    x2 = x + delta[i][0]
-                    y2 = y + delta[i][1]
-                    if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]):
-                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
-                            g2 = g + cost
-                            open.append([g2, x2, y2])
-                            expand[x2][y2] = g2
-                            closed[x2][y2] = 1
+
+
+            for i in range(len(delta)):
+                x2 = x + delta[i][0]
+                y2 = y + delta[i][1]
+                if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]):
+                    if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                        g2 = g + cost
+                        open.append([g2, x2, y2])
+                        expand[x2][y2] = g2
+                        closed[x2][y2] = 1
     #for i in range(len(grid)):
      #   print(expand[i])
     return expand
