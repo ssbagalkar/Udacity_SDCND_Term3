68f67abec1913dd42d64c66478aedbf39688af69
Saurabh S Bagalkar <sbagalka@asu.edu>
2017-11-28 10:00:36 +0530

    add mean,stddev and summarize each attribute

--- L3Ch16/predictionexercise/PredictionExercise/classifier.py
+++ L3Ch16/predictionexercise/PredictionExercise/classifier.py
@@ -1,4 +1,4 @@
-
+import math
 
 class GNB(object):
 
@@ -12,16 +12,43 @@ class GNB(object):
 		INPUTS
 		data - array of N observations
 		  - Each observation is a tuple with 4 values: s, d, 
-		    s_dot and d_dot.
+			s_dot and d_dot.
 		  - Example : [
-			  	[3.5, 0.1, 5.9, -0.02],
-			  	[8.0, -0.3, 3.0, 2.2],
-			  	...
-		  	]
+				[3.5, 0.1, 5.9, -0.02],
+				[8.0, -0.3, 3.0, 2.2],
+				...
+			]
 
 		labels - array of N labels
 		  - Each label is one of "left", "keep", or "right".
+
 		"""
+
+		## Here we separate data according to classes
+		def separateByClass(data,labels):
+			separated = {}
+			for i in range(len(data)):
+				vector = data[i]
+				if (labels[i] not in separated):
+					separated[labels[i]] = []
+				separated[labels[i]].append(vector)
+			return separated
+
+		## Calculate mean and standard deviation of the data
+		def mean(numbers):
+			return sum(numbers)/float(len(numbers))
+
+		def stdev(numbers):
+			avg = mean(numbers)
+			variance = sum([pow(x - avg, 2) for x in numbers]) / float(len(numbers) - 1)
+			return math.sqrt(variance)
+
+
+		def summarize(data):
+			summaries = [(mean(attribute), stdev(attribute)) for attribute in zip(*data)]
+			return summaries
+
+		summarizeData = summarize(data)
 		pass
 
 	def predict(self, observation):
