9e840c45d574961b65111515690ce87e9f994365
Saurabh S Bagalkar <sbagalka@asu.edu>
2017-11-30 16:16:50 +0530

    add all files

---
+++ L3Ch16-cpp/NaiveBayesPredicition/NaiveBayesPredicition/classifier.h
@@ -0,0 +1,48 @@
+#ifndef CLASSIFIER_H
+#define CLASSIFIER_H
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <math.h>
+#include <vector>
+#include "Dense"
+
+using namespace std;
+using Eigen::ArrayXd;
+
+class GNB {
+public:
+
+	vector<string> possible_labels = { "left","keep","right" };
+
+	ArrayXd left_means;
+	ArrayXd left_sds;
+
+	ArrayXd keep_means;
+	ArrayXd keep_sds;
+
+	ArrayXd right_means;
+	ArrayXd right_sds;
+
+
+	/**
+	* Constructor
+	*/
+	GNB();
+
+	/**
+	* Destructor
+	*/
+	virtual ~GNB();
+
+	void train(vector<vector<double> > data, vector<string>  labels);
+
+	string predict(vector<double>);
+
+};
+
+#endif
+
+
+
+#pragma once
